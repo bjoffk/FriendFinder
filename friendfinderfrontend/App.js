9f45c47c8e4ddf7324db7aff5d3a9da7a78fd307
Bearukun <bearukun@gmail.com>
2017-11-08 14:05:00 +0100

    GPS is working now, and things be flexing

--- friendfinderfrontend/App.js
+++ friendfinderfrontend/App.js
@@ -1,43 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MapView } from 'expo';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { MapView, Constants, Location, Permissions } from 'expo';
+
+const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
+const URL = 'https://l.facebook.com/l.php?u=https%3A%2F%2F74e889f3.ngrok.io%2F&h=ATP_HgsLPI3IySkzoT7DziQwdnAEYqSggQ8IeUjhyYjNQ_hjGozBT3wdfMDuwZ-aIs_mKUmqA7Zfq7-fcb_KCuptZklgevlp7rm2WIPk3hJNlWbk0vaBt9M1WmGL-iqqgD4JE-aHNDXHN5SXDxo';
 
 export default class App extends React.Component {
+  state = {
+    location: { coords: { latitude: 0, longitude: 0 } },
+    errorMessage: null,
+    markers: [],
+  };
+
+  componentWillMount() {
+    Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+  }
+
+  locationChanged = (location) => {
+    region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.05,
+    },
+      this.setState({ location, region })
+  }
+
+  getInitialState() {
+    return {
+      markers: []
+    };
+  }
+
+
+
+
   render() {
+
+
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 55.7705449,//lat 55.7705449 Lyngby
-          longitude: 12.5118374, //lon 12.5118374 Lyngby
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <MapView.Marker
-            coordinate={{latitude: 55.7705449,
-            longitude: 12.5118374}}
-            title={"Cphbusiness Lyngby"}
-            description={"Educational Academy"}
-            pinColor= 'turquoise'
+      <View style={{ flex: 0.7 }} >
 
+        <MapView
+          style={{ flex: 0.7 }}
 
+          showsUserLocation={true}
+          region={this.state.region}
+        >
+          {this.state.markers.map(marker => {
+            return (
+              <MapView.Marker
+                coordinate={{
+                  latitude: 55.7705449,
+                  longitude: 12.5118374
+                }}
+                title={"Cphbusiness Lyngby"}
+                description={"Educational Academy"}
+                pinColor='turquoise'
 
-
-/*
-pinColor options 
-red (default), tomato, orange, yellow, gold, wheat, tan, linen, green,
-blue / navy, aqua / teal / turquoise, violet / purple / plum, indigo
-*/
-
-/*
-Ceo Test Comment
-*/
-
-
-        />
-      </MapView>
-
+              />
+            );
+          })}
+        </MapView>
+        <Text>'hej'</Text>
+      </View>
     );
   }
 }
